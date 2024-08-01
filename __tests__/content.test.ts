7763dd9c0db78bac40e1eaa43a770d660b86162d
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-08-01 20:47:42 +0700

    chore: improve content tests

--- __tests__/content.test.ts
+++ __tests__/content.test.ts
@@ -1,4 +1,5 @@
-import { expect, describe, it } from "vitest";
+import { expect, describe, it, beforeAll } from "vitest";
+import * as cheerio from "cheerio";
 
 import { getPostsMetadataByAuthor } from "../utils";
 import {
@@ -15,75 +16,58 @@ const postsMetadataByAuthor = await getPostsMetadataByAuthor({
 
 const postsMetadata = (postsMetadataByAuthor as PostsMetadataByAuthor)[AUTHOR];
 
-// ------------------------------
-// Assertion helpers
-// ------------------------------
-const getHeading = (html: string) => {
-  const match = html.match(/<h1.*data-test-label="post-full-title">.*<\/h1>/);
-  return match ? match[0] : null;
-};
-
-const getAuthorCard = (html: string) => {
-  const match = html.match(
-    /<section.*data-test-label="author-card">([\s\S]*?)<\/section>/
-  );
-  return match ? match[0] : null;
-};
-
-const getAuthorLink = (html: string) => {
-  const match = html.match(
-    /<a.*data-test-label="profile-link".*>([\s\S]*?)<\/a>/
-  );
-  return match ? match[0] : null;
-};
-
-const getContent = (html: string) => {
-  const match = html.match(
-    /<section.*data-test-label="post-content">([\s\S]*?)<\/section>/
-  );
-  return match ? match[0] : null;
-};
-
-const getFeatureImage = (html: string) => {
-  const match = html.match('<img.*data-test-label="feature-image".*?>');
-  return match ? match[0] : null;
-};
-
-const getTweetButton = (html: string) => {
-  const match = html.match(
-    '<button.*data-test-label="tweet-button".*>.*?</button>'
-  );
-  return match ? match[0] : null;
-};
-
-// This test is probably flaky / can have false negatives since it compares strings
-// rather than querying the elements and looking for their attributes.
 describe.each(EXPECTED_POST_URLS)("%s - Post content", (url) => {
   const { html: expectedHtml } = EXPECTED_POSTS_METADATA[url];
   const { html } = postsMetadata[url];
 
+  let $html: ReturnType<typeof cheerio.load>;
+  let $expectedHtml: ReturnType<typeof cheerio.load>;
+
+  beforeAll(() => {
+    $html = cheerio.load(html);
+    $expectedHtml = cheerio.load(expectedHtml);
+  });
+
   it("should have the correct heading", () => {
-    expect(getHeading(html)).toBeTruthy();
-    expect(getHeading(html)).toEqual(getHeading(expectedHtml));
+    const query = "h1";
+
+    expect($html(query)).toBeTruthy();
+    expect($html(query).length).toEqual(1);
+    expect($html(query).text()).toEqual($expectedHtml(query).text());
   });
 
   it("should have the correct author card", () => {
-    expect(getAuthorCard(html)).toBeTruthy();
-    expect(getAuthorCard(html)).toEqual(getAuthorCard(expectedHtml));
+    const query = "section[data-test-label='author-card']";
+
+    expect($html(query)).toBeTruthy();
+    expect($html(query).length).toEqual(2);
+    expect($html(query).text()).toEqual($expectedHtml(query).text());
   });
 
   it("should have the correct author link", () => {
-    expect(getAuthorLink(html)).toBeTruthy();
-    expect(getAuthorLink(html)).toEqual(getAuthorLink(expectedHtml));
+    const query = "a[data-test-label='profile-link']";
+
+    expect($html(query)).toBeTruthy();
+    expect($html(query).length).toEqual(2);
+    expect($html(query).text()).toEqual($expectedHtml(query).text());
   });
 
   it("should have the correct feature image", () => {
-    expect(getFeatureImage(html)).toBeTruthy();
-    expect(getFeatureImage(html)).toEqual(getFeatureImage(expectedHtml));
+    const query = "img[data-test-label='feature-image']";
+
+    expect($html(query)).toBeTruthy();
+    expect($html(query).length).toEqual(1);
+    expect($html(query).attr("src")).toEqual($expectedHtml(query).attr("src"));
+    expect($html(query).attr("alt")).toEqual($expectedHtml(query).attr("alt"));
   });
 
   it("should have the correct tweet button", () => {
-    expect(getTweetButton(html)).toBeTruthy();
-    expect(getTweetButton(html)).toEqual(getTweetButton(expectedHtml));
+    const query = "button[data-test-label='tweet-button']";
+
+    expect($html(query)).toBeTruthy();
+    expect($html(query).length).toEqual(1);
+    expect($html(query).attr("onclick")).toEqual(
+      $expectedHtml(query).attr("onclick")
+    );
   });
 });
